ef5230be7f64ac1a286196aa6d79b31d29d8d36f
Balaharinath C <balaharinath.dev@gmail.com>
2025-03-01 21:47:37 +0530

    Completed

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ if uploaded_file and st.button('Convert'):
                 content=[
                     {
                         'type': 'text',
-                        'text': 'You are an expert AI who summarizes from a given json data into bullet points which is purely in context of medical science and also translates them into suitable language!'
+                        'text': 'You are an expert AI who summarizes from a given json data into paras and bullet points which is purely in context of medical science and also translates them into suitable language!'
                     }
                 ]
             ),
@@ -85,7 +85,7 @@ if uploaded_file and st.button('Convert'):
                 content=[
                     {
                         'type': 'text',
-                        'text': f'Summarize into bullet points in medical context for the given json translated in {language}: \n {response}.'
+                        'text': f'Summarize into paras and bullet points in medical context for the given json translated in {language}: \n {response}.'
                     }
                 ]
             )
